b35f7e0b046b839e3cc115a2e1a2e19dbbfb975c
akashp69 <akashpakhare499@gmail.com>
2022-07-19 00:45:47 +0530

    [Akash] Added solutions for Day5 Problems01

---
+++ 5Random2Digit.sh
@@ -0,0 +1,17 @@
+#! /bin/bash -x
+
+a=$((RANDOM%10 + 10 ))
+b=$((RANDOM%10 + 10 ))
+c=$((RANDOM%10 + 10 ))
+d=$((RANDOM%10 + 10 ))
+e=$((RANDOM%10 + 10 ))
+
+   sum=$((a+b+c+d+e))
+   avg=$((sum/5))
+
+   echo=$sum
+   echo=$avg
+
+
+
+
---
+++ AddRandom2Dice.sh
@@ -0,0 +1,8 @@
+#! /bin/bash -x
+
+Dice1=$((RANDOM%6))
+Dice2=$((RANDOM%6))
+
+sum=$((Dice1+Dice2))
+
+echo=$sum
